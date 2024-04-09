05c686d5264c972a4c2150bc56d6e4de35d0f7bd
saasc <148303996+Fahmithasathik@users.noreply.github.com>
2024-04-09 19:34:37 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,6 @@
+<h2><b>About Us</b></h2>
+<h5>
+<p align="justify">Campus 360 admin can send the notification to the particular students regarding fee payments, results, any new activity happen in college campus or college fest participation, libraries dues, hostel room payments, any workshop registrations, warnings and reminders etc. for this work Campus 360 project is make all work much easier and understandable to all.
+This Campus 360 work generally intends to act as a support system for the all users. By the help of free Campus 360, users can access the notifications and articles quickly not only in the particular premises, also wherever and whenever they need to know.Campus 360 usability is fully capable of passing relevant notices and announcements and keeping the users update from time to time.
+</p>
+</h5>
---
+++ contact.php
@@ -0,0 +1,49 @@
+<h2><b>Quick Contact</b></h2>
+<form method="post">
+	<div class="row">
+		<div class="col-md-8">
+		<div class="row">
+		<div class="col-sm-4"></div>
+		<div class="col-sm-4"><?php echo @$err;?></div>
+	</div>
+	
+	
+	
+	<div class="row">
+		<div class="col-sm-4">Enter YOur Email</div>
+		<div class="col-sm-5">
+		<input type="email" name="e" class="form-control"/></div>
+	</div>
+	
+	<div class="row">
+		<div class="col-sm-4">Enter YOur Mobile</div>
+		<div class="col-sm-5">
+		<input type="number" name="n" class="form-control"/></div>
+	</div>
+	
+	<div class="row">
+		<div class="col-sm-4">Enter YOur Message</div>
+		<div class="col-sm-5">
+		<input type="text" name="n" class="form-control"/></div>
+	</div>
+	
+	<div class="row" style="margin-top:10px">
+		<div class="col-sm-2"></div>
+		<div class="col-sm-8">
+		<input type="submit" value="Send Query" name="save" class="btn btn-success"/>
+		
+		</div>
+	</div>
+		
+		</div>
+		<div class="col-md-4">
+		<h2><b>Contact us</b></h2>
+		
+		Mobile:6369581432 <br/>
+		Email:saasc@gmail.com<br/>
+		Website:http/:campus360
+		</div>
+	</div>
+	
+	
+</form>	
